f2cef023aaa8b128c14b99dec80529a23eb98cbe
Luca Piazzoni <luca@creeostudio.it>
2019-04-12 09:26:32 +0200

    Updates

---
+++ precache-manifest.10ddf15e8da48d1c1c045332e37fa175.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6660ab580a067f78b1ce",
+    "url": "/monthly-situation/static/js/runtime~main.5d466542.js"
+  },
+  {
+    "revision": "507296df038fc303a86a",
+    "url": "/monthly-situation/static/js/main.59145cf1.chunk.js"
+  },
+  {
+    "revision": "9d44edbdfbc723c92e67",
+    "url": "/monthly-situation/static/js/2.bf641f49.chunk.js"
+  },
+  {
+    "revision": "507296df038fc303a86a",
+    "url": "/monthly-situation/static/css/main.be1f1c9d.chunk.css"
+  },
+  {
+    "revision": "66977489050f207f5126c006fe6178e1",
+    "url": "/monthly-situation/index.html"
+  }
+];
